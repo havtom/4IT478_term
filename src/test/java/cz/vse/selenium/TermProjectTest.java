7063c394dfdd84757b0534523947a4ac84dc8104
Tomáš Havrda <tomas.havrda@avast.com>
2020-05-26 15:45:30 +0200

    Grid simplification

--- src/test/java/cz/vse/selenium/TermProjectTest.java
+++ src/test/java/cz/vse/selenium/TermProjectTest.java
@@ -15,6 +15,7 @@ import java.text.ParseException;
 import java.util.UUID;
 
 import static cz.churchcrm.testframework.utils.TestUtils.changeDateFormat;
+import static org.junit.Assert.assertTrue;
 
 public class TermProjectTest extends BaseTest{
 
