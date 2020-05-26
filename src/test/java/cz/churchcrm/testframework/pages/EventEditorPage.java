11e472c1faafbd972cca8e94d15dd6d5fe5bb1c8
linm02 <linm02@vse.cz>
2020-05-26 22:44:16 +0200

    finished CreateEventTest

--- src/test/java/cz/churchcrm/testframework/pages/EventEditorPage.java
+++ src/test/java/cz/churchcrm/testframework/pages/EventEditorPage.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.support.ui.Select;
 
 public class EventEditorPage extends Page {
 
-    private static final String URL = "https://digitalnizena.cz/church/EventEditor.php";
-    private static final String CSS_SELECTOR_EVENT_TITLE = "[name='EventTitle']";
-    private static final String CSS_SELECTOR_EVENT_DESC = "[name='EventDesc']";
-    private static final String CSS_SELECTOR_EVENT_TOTAL_COUNT = "[name='EventCount[]']";
-    private static final String CSS_SELECTOR_EVENT_STATUS_INACTIVE = "[name='EventStatus'][value='1']";
-    private static final String CSS_SELECTOR_SAVE_EVENT_BTN = "[name='SaveChanges']";
+    public static final String URL = "https://digitalnizena.cz/church/EventEditor.php";
+    public static final String CSS_SELECTOR_EVENT_TITLE = "[name='EventTitle']";
+    public static final String CSS_SELECTOR_EVENT_DESC = "[name='EventDesc']";
+    public static final String CSS_SELECTOR_EVENT_TOTAL_COUNT = "[name='EventCount[]']";
+    public static final String CSS_SELECTOR_EVENT_STATUS_INACTIVE = "[name='EventStatus'][value='1']";
+    public static final String CSS_SELECTOR_SAVE_EVENT_BTN = "[name='SaveChanges']";
     public static final String CSS_SELECTOR_EVENT_DATE_RANGE = "#EventDateRange";
 
     public EventEditorPage(WebDriver driver) {
@@ -21,14 +21,10 @@ public class EventEditorPage extends Page {
     }
 
     public void createNewEvent(EventType eventType) {
-//        driver.findElement(By.cssSelector());
 
         WebElement classificationSelectElement = driver.findElement(By.cssSelector("#event_type_id"));
         Select classificationSelect = new Select(classificationSelectElement);
         classificationSelect.selectByIndex(eventType.nr);
-        //???
-//        WebElement searchInput = driver.findElementByName("q");
-
     }
 
     public void editTitle(String s) {
@@ -56,11 +52,8 @@ public class EventEditorPage extends Page {
     }
 
     public void addIntoIFrame(String s) {
-
-        WebElement areaInIFrame = driver.findElement(By.cssSelector("#cke_1_contents"));
         WebElement frame = driver.switchTo().frame(0).findElement(By.cssSelector("body"));
-//        WebElement frame = driver.switchTo().frame(0).findElement(By.cssSelector("#cke_1_contents"));
-        frame.sendKeys("ffsdfsdfsd");
+        frame.sendKeys(s);
         driver.switchTo().parentFrame();
     }
 
